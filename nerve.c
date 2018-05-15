aa5503f27f1ee0fae8a4fec09cd44fb7274fc0f9
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-05-15 02:04:07 +0200

    Add socket creation.

--- nerve.c
+++ nerve.c
@@ -6,5 +6,11 @@
 
 int main(void)
 {
+	int socket_desc;
+
+	socket_desc = socket(AF_INET6, SOCK_STREAM, 0);
+	if (socket_desc == -1)
+		printf("Error %d : could not create socket.\n", errno);
+
 	return 0;
 }
