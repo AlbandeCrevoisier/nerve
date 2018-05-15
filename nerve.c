10346fa4596275a9992f88b49a998c023e092cbe
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-05-15 02:17:57 +0200

    Improve naming. A bit.

--- nerve.c
+++ nerve.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 	int sockfd;
-	struct sockaddr_in6 homeserver;
+	struct sockaddr_in6 addr;
 
 	sockfd = socket(AF_INET6, SOCK_STREAM, 0);
 	if (socket_desc == -1)
 		printf("Error %d : could not create socket.\n", errno);
 
-	homeserver.sin6_port = (in_port_t) 8448;
-	homeserver.sin6_addr = (struct in6_addr) { "::1" };
+	addr.sin6_port = (in_port_t) 8448;
+	addr.sin6_addr = (struct in6_addr) { "::1" };
 
 	return 0;
 }
