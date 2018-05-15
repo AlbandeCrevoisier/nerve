67a8e4b96f444089e55563b51efb17a81ed09365
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-05-15 02:06:52 +0200

    Add socket address struct & port.

--- nerve.c
+++ nerve.c
@@ -3,14 +3,18 @@
 #include <stdio.h>
 #include <errno.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(void)
 {
 	int socket_desc;
+	struct sockaddr_in6 homeserver;
 
 	socket_desc = socket(AF_INET6, SOCK_STREAM, 0);
 	if (socket_desc == -1)
 		printf("Error %d : could not create socket.\n", errno);
+
+	homeserver.sin6_port = (in_port_t) 8448;
 
 	return 0;
 }
