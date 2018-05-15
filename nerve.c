6fccf88edd8a763c190f82a7c49832a79c2833c5
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-05-15 02:02:50 +0200

    Add matrix client playground skel.

---
+++ nerve.c
@@ -0,0 +1,10 @@
+/* Matrix client playground ***************************************************/
+
+#include <stdio.h>
+#include <errno.h>
+#include <sys/socket.h>
+
+int main(void)
+{
+	return 0;
+}
