3155209ef1624f58afc9a22c4876b973e1b8b14a
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-05-28 01:54:12 +0200

    Complete initialisation, correct message length.

--- nerve.c
+++ nerve.c
@@ -5,12 +5,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(void)
 {
 	int sockfd;
 	struct sockaddr_in6 addr;
-	const char *msg = "Hello, world!\n";
+	const char *msg = "Hello, world\n";
 
 	sockfd = socket(AF_INET6, SOCK_STREAM, 0);
 	if (sockfd == -1) {
@@ -18,15 +19,18 @@ int main(void)
 		return errno;
 	}
 
-	addr.sin6_port = (in_port_t) htons(9999);
+	addr.sin6_family = AF_INET6;
+	addr.sin6_port = htons(9999);
+	addr.sin6_flowinfo = 0;
 	addr.sin6_addr = in6addr_loopback;
+	addr.sin6_scope_id = 0;
 
 	if (connect(sockfd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
 		printf("Error %d: could not connect socket.\n", errno);
 		return errno;
 	}
 
-	if (send(sockfd, msg, sizeof(msg), 0) == -1) {
+	if (send(sockfd, msg, strlen(msg)+1, 0) == -1) {
 		printf("Error %d: could not send message.\n", errno);
 		return errno;
 	}
