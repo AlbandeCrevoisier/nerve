ac45e4b211372134a104b3a496a30857920b365d
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-05-28 21:15:54 +0200

    Read the answer.

--- nerve.c
+++ nerve.c
@@ -11,7 +11,9 @@ int main(void)
 {
 	int sockfd;
 	struct sockaddr_in6 addr;
-	const char *msg = "Hello, world\n";
+	char in[1024], out[1024];
+	int r, rcv, rest;
+	const char* msg = "GET / HTTP/1.1\r\n\r\n";
 
 	sockfd = socket(AF_INET6, SOCK_STREAM, 0);
 	if (sockfd == -1) {
@@ -20,7 +22,7 @@ int main(void)
 	}
 
 	addr.sin6_family = AF_INET6;
-	addr.sin6_port = htons(9999);
+	addr.sin6_port = htons(8008);
 	addr.sin6_flowinfo = 0;
 	addr.sin6_addr = in6addr_loopback;
 	addr.sin6_scope_id = 0;
@@ -30,10 +32,31 @@ int main(void)
 		return errno;
 	}
 
-	if (send(sockfd, msg, strlen(msg)+1, 0) == -1) {
-		printf("Error %d: could not send message.\n", errno);
+	strncpy(out, msg, strlen(msg)+1);
+
+	if (write(sockfd, out, strlen(out)+1) == -1) {
+		printf("Error %d: could not write message.\n", errno);
 		return errno;
 	}
+
+	rcv = 0;
+	rest = sizeof(in)-1;
+	do {
+		if((r = read(sockfd, in+rcv, rest)) == -1 ) {
+			printf("Error %d: could not read message.\n", errno);
+			return errno;
+		}
+		rcv += r;
+		rest -= r;
+		if (rest < 1) {
+			printf("Error: server message longer than buffer.\n");
+			return -1;
+		}
+	} while (r != 0);
+
+	printf("%d\n", rcv);
+	strncpy(in, in, strlen(in)+1);
+	printf("%s\n", in);
 
 	close(sockfd);
 
